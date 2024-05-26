6c7188d54d19b239a5ffcc287241f7b58a9bf85d
dongjun Noh <bluecode671@gmail.com>
2024-05-26 12:20:24 +0900

    first

---
+++ app/src/main/java/com/example/pmf/ui/main/MainViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.pmf.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
